7d1ff63cae7cc58cd691021de9d8a89fc30d9aff
Calvin Phuong <phuong24@up.edu>
2023-11-29 22:35:07 -0800

    Project Finished and working | Could be commented more later

--- Union.py
+++ Union.py
@@ -10,73 +10,112 @@ import os
 #import pydot
 #import graphviz
 
-# Checks if files are valid in the system
+# Name: validFile
+# Parameters: file - a file in the system
+# Description: This function checks if the file provided exists, can be
+#              accessed, is the correct file type, and if the file is empty
+
 def validFile(file):
 
     if(not os.path.isfile(file)):
         print("Error: File does not exist\n")
-        return None
+        sys.exit()
     if(not os.access(file, os.R_OK)):
         print("Error: File cannot be accessed\n")
-        return None
+        sys.exit()
     if(file[-5:] != ".json"):
         print("Error: Invalid file type\n")
-        return None
+        sys.exit()
 
     if(os.stat(file).st_size == 0):
         print("Error: File is empty\n")
-        return None
+        sys.exit()
 
     else:
         print(file, "File exists\n")
+        
 
-### COME BACK TO THIS IF NEEDED ###
-""" def createJSON(filename):
-     f = open(filename, "w")
+# Name: createJSON
+# Parameters: infile - a json object
+# Description: This function creates a new json file with write permissions
+#              and writes the infile data the new json file
 
-     f.close() """
+def createJSON(infile):
+    filename = "unioned DFA.json"
+    with open(filename, "w") as outfile:
+        outfile.write(infile)
+
+
+    outfile.close()
 
 def createDict(file1, file2):
     # creates new dictionary for final DFA
     new_dict = {}
+    num = len(file1) * len(file2)
     
-    # iterates through DFAs to fill out dictionary
+    # iterates through DFAs to create new states
     for i in file1:
         for j in file2:
             new_dict[i + j] = {}
 
-            ### ENDED HERE DOO DOO ###
-            if (i.type == 'start' or j.get('type') == 'start'):
-                new_dict[i+j]['type'] = 'start'
+            # adds start state for final DFA
+            if (file1['q0']['start'] == 'true' and file2['q0']['start'] == 'true'):
+                new_dict['q0q0']['start'] = 'true'
 
+            # check if either DFA 1 or DFA 2 have an accepting state
+            # if so, set state as accept state in final DFA
+            # else, add reject state
+            if (file1[i]['type'] == 'accept' or file2[j]['type'] == 'accept'):
+                new_dict[i + j]['type'] = 'accept'
+            else:
+                new_dict[i + j]["type"] = 'reject'
 
-    print(new_dict)
+            # add transitions between states 
+            new_dict[i+j]['next_a'] = file1[i]['next_a'] + file2[j]['next_a']
+            new_dict[i+j]['next_b'] = file1[i]['next_b'] + file2[j]['next_b']
+
+            
     
     return new_dict
+
+def totalStates(file1, file2):
+     
+     return len(file1) * len(file2)
 
 
 def main():
     # gets arguments
     args = sys.argv[0:]
 
-    # checks if DFA files are valid
-    file1 = args[1]
-    validFile(file1)
+    # checks if DFA files are valid and are provided arguments
+    if len(sys.argv) != 3:
+        print('Error: Please enter 2 valid files\n')
 
-    file2 = args[2]
-    validFile(file2)
+    # continue with program
+    else:
 
-    # opens files to use 
-    with open(file1) as f1:
-        data1 = json.load(f1)
-        with open(file2) as f2:
-            data2 = json.load(f2)
-            
-            peepee = createDict(data1,data2)
+        # checks if provided files are valid
+        file1 = args[1]
+        validFile(file1)
 
-    # closes files that were used
-    f1.close()
-    f2.close()
+        file2 = args[2]
+        validFile(file2)
+
+        # opens files to use 
+        with open(file1) as f1:
+            data1 = json.load(f1)
+            with open(file2) as f2:
+                data2 = json.load(f2)
+                
+                peepee = createDict(data1,data2)
+
+                pp = json.dumps(peepee, indent = totalStates(data1,data2))
+                createJSON(pp)
+                print(pp)
+
+        # closes files that were used
+        f1.close()
+        f2.close()
 
 
 
